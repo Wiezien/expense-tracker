18d3b884994e3547be4ab2a31ac41aebd7b578b9
Wiezien <145956394+Wiezien@users.noreply.github.com>
2024-07-12 06:21:57 +0100

    tidy up

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import AddTransaction from "./AddTransaction";
-import OverviewComponent from "./OverviewComponent";
-import TransactionsContainer from "./TransactionsContainer";
+import AddTransaction from "../components/AddTransaction";
+import OverviewComponent from "../components/OverviewComponent";
+import TransactionsContainer from "../components/TransactionsContainer";
 
 const Container = styled.div`
   display: flex;
